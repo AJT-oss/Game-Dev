bd0308e1bddce257beaf757d1bd7420868821eff
Arjun <arjun.thadathil@aics.espritscholen.nl>
2026-02-16 17:57:28 +0100

    Add files via upload

---
+++ tupples.py
@@ -0,0 +1,26 @@
+number=(1,2,3,4,5)
+print(number)
+
+lang=("python","HTML","CSS","Javascript","Csharp")
+
+print(lang[2])
+print(lang[3])
+
+print(len(lang))
+
+for item in lang:
+    print(item)
+
+colors=("red","blue","green","yellow","orange","purple","pink",)
+
+print("pink" in colors)
+
+combined=(lang+colors)
+print(combined)
+
+tupple1=(1,2,3,4)
+rep=tupple1*10
+print(rep)
+
+print(rep.count(3))
+print(rep.index(2))
