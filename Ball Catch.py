a61170148d15b9c9221e45d5399933d08de3caf1
Arjun <arjun.thadathil@aics.espritscholen.nl>
2026-04-20 17:54:49 +0200

    Add files via upload

--- Ball Catch.py
+++ Ball Catch.py
@@ -11,21 +11,45 @@ basket= Actor("basket.png")
 basket.pos=200,550
 
 score=0
+lives=3
 
 def draw() :
     screen.blit("bg.png",(0,0))
     ball.draw()
     basket.draw()
     screen.draw.text(f"Score:{score}",(25,15))
+    screen.draw.text(f"Lives:{lives}",(250,15))
+
+fallingspeed = 7
 
 
 def update():
     global score,c
+    global lives
     if keyboard.left:
         basket.x-=5
     if keyboard.right:
         basket.x+=5
-        
+    
+    ball.y+=fallingspeed
+    if ball.y>HEIGHT:
+        lives-=1
+        resetball()
+
+
+    
+
+    if basket.colliderect(ball):
+        score+=1
+        resetball()
+
+    if lives<1:
+        screen.draw.text("Game Over",(300,200))
+
+
+def resetball():
+    ball.pos= (random.randint(30,WIDTH-30),0)
+    
 
 
 
